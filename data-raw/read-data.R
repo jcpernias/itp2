0b8efe77e116ce76a63d75468fd85e4b2cb8ab76
José C. Pernías <jcpernias@gmail.com>
2024-01-14 20:18:16 +0100

    Add code to create factors

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 library(glue)
 
-variables <- read_csv("./data-raw/variables.csv", col_types = "ccc")
-absent <- read_csv("./data-raw/absent.csv", col_types = "ci")
+## Utility functions
 
-ecv_files <- expand_grid(type = c("d", "h", "p", "r"),
-                         year = c("05", "11", "19"))
+data_file_path <- function(x) {
+  file.path("./data-raw", x)
+}
 
-file_names <-  unlist(pmap(ecv_files, \(type, year) {
-  file.path("./data-raw", glue("esudb{year}{type}.csv.gz"))
-}))
 
-## TODO: handle absent argument∫
+## Read data files
+
+variables <- read_csv(data_file_path("variables.csv"), col_types = "ccc")
+absent <- read_csv(data_file_path("absent.csv"), col_types = "ci")
+
 read_ecv_file <- function(type, year, variables, absent) {
-  file_name <- file.path("./data-raw",
-                         glue("esudb{year}{type}.csv.gz"))
+  file_name <- data_file_path(glue("esudb{year}{type}.csv.gz"))
   file_vars <- variables |> filter(file == .env$type)
   var_list <- as.list(file_vars$type) |> setNames(file_vars$name)
   var_list$.default = "-"
@@ -24,9 +24,42 @@ read_ecv_file <- function(type, year, variables, absent) {
   read_csv(file_name, col_types = var_list[!exclude])
 }
 
-db <- read_ecv_file("d", "05", variables, absent)
 
-db <- read_ecv_file("p", "05", variables, absent)
+ecv_files <- expand_grid(type = c("d", "h", "p", "r"),
+                         year = c("05", "11", "19"))
 
-absent |> filter(year == 2005) |> pull(var)
-db |> colnames() %in% (absent |> filter(year == 2005) |> pull(var))
+pwalk(ecv_files, \(type, year) {
+  assign(glue("ecv{year}{type}"),
+         read_ecv_file(type, year, variables, absent),
+         envir = .GlobalEnv)
+})
+
+
+## Create factors
+
+factors_db <- read_csv(data_file_path("factors.csv"), col_types = "cc")
+
+make_factor_table <- function(factor_name, factors_db) {
+  factor_type <- factors_db |>
+    filter(factor == factor_name) |>
+    pull(type)
+  read_csv(data_file_path(glue("{factor_name}.csv")),
+           col_types = glue("c{factor_type}"))
+}
+
+make_factor <- function(x, factor_table) {
+  f <- as_tibble_col(x) |>
+    left_join(factor_table, join_by(value == ecv)) |>
+    pull(levels)
+  factor(f, levels = factor_table$levels)
+}
+
+
+as_tibble_col(ecv05p |> pull(PM100)) |>
+  left_join(fin_hardship_tbl, join_by(value == ecv))
+
+fin_hardship_tbl <- make_factor_table("fin_hardship", factors_db)
+
+f <- make_factor(ecv05p |> pull(PM100), fin_hardship_tbl)
+
+summary(f)
