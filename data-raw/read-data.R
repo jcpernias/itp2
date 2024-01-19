23a7729c7b92ab9d55785903c0a0e82ce3aba3d9
José C. Pernías <jcpernias@gmail.com>
2024-01-19 13:44:38 +0100

    Add variables to individuals database

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -37,8 +37,6 @@ pwalk(ecv_files, \(type, year) {
 
 ## Create factors
 
-factors_db <- read_csv(data_file_path("factors.csv"), col_types = "cc")
-
 make_factor_table <- function(factor_name, factors_db) {
   factor_type <- factors_db |>
     filter(factor == factor_name) |>
@@ -51,7 +49,7 @@ make_factor <- function(x, factor_table) {
   f <- as_tibble_col(x) |>
     left_join(factor_table, join_by(value == ecv)) |>
     pull(levels)
-  factor(f, levels = factor_table$levels)
+  factor(f, levels = unique(factor_table$levels))
 }
 
 ## Fix PE040 in 2019 database
@@ -75,11 +73,13 @@ ecv_pp <- left_join(ecv_all_r, ecv_all_p,
 ## Final databases
 ##
 
+factors_db <- read_csv(data_file_path("factors.csv"), col_types = "cc")
 fin_hardship_tbl <- make_factor_table("fin_hardship", factors_db)
 region_tbl <- make_factor_table("region", factors_db)
 urb_tbl <- make_factor_table("urb", factors_db)
 type_hh_tbl <- make_factor_table("type_hh", factors_db)
-
+sex_tbl <- make_factor_table("sex", factors_db)
+nation_tbl <- make_factor_table("nation", factors_db)
 
 
 households <- ecv_hh |>
@@ -93,3 +93,30 @@ households <- ecv_hh |>
             ydisp_hh = vhRentaa,
             pov_hh = vhPobreza == 1,
             depriv_hh = vhMATDEP == 1)
+
+individuals <- ecv_pp |>
+  transmute(id_hh = as.integer((RB010 - 2000) * 10000000 + trunc(RB030 / 100)),
+            id_p = as.integer((RB010 - 2000) * 10000000 + RB030),
+            ecv_year = factor(RB010, levels = c(2005, 2011, 2019)),
+            birth_year = RB080,
+            age = as.integer(RB010 - RB080 - 1),
+            sex = make_factor(RB090, sex_tbl),
+            absent = RB200 == 2,
+            lowjob = vrLOWJOB == 1,
+            eu2020 = vrEU2020 == 1,
+            country_birth = make_factor(PB210, nation_tbl),
+            nationality = make_factor(PB220A, nation_tbl),
+            educ_year = PE030,
+            educ_low = PE040_F == -2 | PE040 %in% c(0, 1, 100),
+            educ_med = PE040 %in% c(2, 3, 200, 300, 344, 353, 354),
+            educ_sup = PE040 %in% c(4, 5, 400, 450, 500),
+            work_age = PL190,
+            work_years = PL200,
+            full_time = PL031 %in% c(1, 3) | PL030 == 1,
+            part_time = PL031 %in% c(2, 4) | PL030 == 2,
+            self = PL040 %in% c(1, 2),
+            worker = PL040 == 3,
+            unempl = PL031 == 5 | PL030 == 3,
+            inactive = PL031 %in% 6:11 | PL030 %in% 4:9,
+            has_worked = PL015 == 1,
+  )
